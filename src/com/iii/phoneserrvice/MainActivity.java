661ce3157e151aab19bf0c4508bf61a382982a9c
tranminhthuan <tranminhthuan@3i.com.vn>
2013-05-27 18:53:13 -0700

    date 27.5

---
+++ src/com/iii/phoneserrvice/MainActivity.java
@@ -0,0 +1,232 @@
+package com.iii.phoneserrvice;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.AsyncFacebookRunner.RequestListener;
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
+
+public class MainActivity extends Activity {
+
+	private static String APP_ID = "466055656814720";
+	private Facebook facebook;
+	@SuppressWarnings("deprecation")
+	private AsyncFacebookRunner mAsyncRunner;
+	String FILENAME = "AndroidSSO_data";
+	private SharedPreferences mPrefs;
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		facebook = new Facebook(APP_ID);
+		mAsyncRunner = new AsyncFacebookRunner(facebook);
+
+		Button btnFbLogin = (Button) findViewById(R.id.button1);
+		Button btnPostToWall = (Button) findViewById(R.id.button2);
+		Button btnFbLogout = (Button) findViewById(R.id.button3);
+		btnFbLogin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				loginToFacebook();
+			}
+		});
+		btnFbLogout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				logoutFromFacebook();
+			}
+		});
+		btnPostToWall.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				postToWall();
+			}
+		});
+	}
+
+	@SuppressWarnings("deprecation")
+	public void postToWall() {
+		// post on user's wall.
+		facebook.dialog(this, "feed", new DialogListener() {
+
+			@Override
+			public void onFacebookError(FacebookError e) {
+			}
+
+			@Override
+			public void onError(DialogError e) {
+			}
+
+			@Override
+			public void onComplete(Bundle values) {
+			}
+
+			@Override
+			public void onCancel() {
+			}
+		});
+
+	}
+
+	@SuppressWarnings("deprecation")
+	public void getProfileInformation() {
+		mAsyncRunner.request("me", new RequestListener() {
+			@Override
+			public void onComplete(String response, Object state) {
+				Log.d("Profile", response);
+				String json = response;
+				try {
+					JSONObject profile = new JSONObject(json);
+					// getting name of the user
+					final String name = profile.getString("name");
+					// getting email of the user
+					final String email = profile.getString("email");
+
+					runOnUiThread(new Runnable() {
+
+						@Override
+						public void run() {
+							Toast.makeText(getApplicationContext(),
+									"Name: " + name + "\nEmail: " + email,
+									Toast.LENGTH_LONG).show();
+						}
+
+					});
+
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+
+			@Override
+			public void onIOException(IOException e, Object state) {
+			}
+
+			@Override
+			public void onFileNotFoundException(FileNotFoundException e,
+					Object state) {
+			}
+
+			@Override
+			public void onMalformedURLException(MalformedURLException e,
+					Object state) {
+			}
+
+			@Override
+			public void onFacebookError(FacebookError e, Object state) {
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@SuppressWarnings("deprecation")
+	public void loginToFacebook() {
+		mPrefs = getPreferences(MODE_PRIVATE);
+		String access_token = mPrefs.getString("access_token", null);
+		long expires = mPrefs.getLong("access_expires", 0);
+
+		if (access_token != null) {
+			facebook.setAccessToken(access_token);
+		}
+
+		if (expires != 0) {
+			facebook.setAccessExpires(expires);
+		}
+
+		if (!facebook.isSessionValid()) {
+			facebook.authorize(this,
+					new String[] { "email", "publish_stream" },
+					new DialogListener() {
+
+						@Override
+						public void onCancel() {
+							// Function to handle cancel event
+						}
+
+						@Override
+						public void onComplete(Bundle values) {
+							// Function to handle complete event
+							// Edit Preferences and update facebook acess_token
+							SharedPreferences.Editor editor = mPrefs.edit();
+							editor.putString("access_token",
+									facebook.getAccessToken());
+							editor.putLong("access_expires",
+									facebook.getAccessExpires());
+
+							editor.commit();
+						}
+
+						@Override
+						public void onError(DialogError error) {
+							// Function to handle error
+
+						}
+
+						@Override
+						public void onFacebookError(FacebookError fberror) {
+							// Function to handle Facebook errors
+
+						}
+
+					});
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	public void logoutFromFacebook() {
+		mAsyncRunner.logout(this, new RequestListener() {
+			@Override
+			public void onComplete(String response, Object state) {
+				Log.d("Logout from Facebook", response);
+				if (Boolean.parseBoolean(response) == true) {
+					Toast.makeText(MainActivity.this, "Logout Success", 1)
+							.show();
+				}
+			}
+
+			@Override
+			public void onIOException(IOException e, Object state) {
+			}
+
+			@Override
+			public void onFileNotFoundException(FileNotFoundException e,
+					Object state) {
+			}
+
+			@Override
+			public void onMalformedURLException(MalformedURLException e,
+					Object state) {
+			}
+
+			@Override
+			public void onFacebookError(FacebookError e, Object state) {
+			}
+		});
+	}
+
+}
